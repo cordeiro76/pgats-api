cb71b7a2750623dad11d4c3d11a06cd7d9ff9ed5
Jessica <cordeiro76@hotmail.com>
2025-09-18 21:08:20 -0300

    Add tests

---
+++ test/external/userExternal.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Checkout Controller', () => {
+    describe('POST /api/users/register', () => {
+        it('Quando é informado dados válidos é cadastrado e recebo 201', async () => {
+            const resposta = await request('http://localhost:3000')
+                .post('/api/users/register')
+                .send({
+                    name: "jessica",
+                    email: "jessica_valid@test.com",
+                    password: "Jessic@12345"
+                });
+
+            expect(resposta.status).to.equal(201);
+            expect(resposta.body).to.have.property('user');
+            expect(resposta.body.user).to.have.property('name', 'jessica');
+            expect(resposta.body.user).to.have.property('email', 'jessica_valid@test.com');
+        });
+    });
+});
